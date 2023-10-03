fc13249e75cc08a78517eab2a21db1e7b2932ffe
HaniChihabi <hanichihabih@gmail.com>
2023-10-03 13:47:43 +0200

    Player drawn

--- src/pacman.py
+++ src/pacman.py
@@ -13,7 +13,14 @@ font = pygame.font.Font('freesansbold.ttf', 20)
 level = boards
 color = 'blue'
 PI = math.pi
- 
+player_images = []
+for i in range(1, 5):
+    player_images.append(pygame.transform.scale(pygame.image.load(f'assets/player_images/{i}.png'), (45, 45)))
+player_x = 450
+player_y = 663
+direction = 0
+counter = 0
+
 def draw_board():
    num1 = ((HEIGHT - 50) // 32)
    num2 = (WIDTH // 30)
@@ -47,11 +54,24 @@ def draw_board():
            if level[i][j] == 9:
                pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
                                 (j * num2 + num2, i * num1 + + (0.5 * num1)), 3)
+               
+def draw_player():
+    # 0-RIGHT, 1-LEFT, 2-UP, 3-DOWN
+    if direction == 0:
+        screen.blit(player_images[counter // 5], (player_x, player_y))
+    elif direction == 1:
+        screen.blit(pygame.transform.flip(player_images[counter // 5], True, False), (player_x, player_y))
+    elif direction == 2:
+        screen.blit(pygame.transform.rotate(player_images[counter // 5], 90), (player_x, player_y))
+    elif direction == 3:
+        screen.blit(pygame.transform.rotate(player_images[counter // 5], 270), (player_x, player_y))
+
 run = True
 while run:
     timer.tick(fps)
     screen.fill('black')
     draw_board()
+    draw_player()
 
     for event in pygame.event.get():
        if event.type == pygame.QUIT:
@@ -61,5 +81,3 @@ while run:
 pygame.quit()
 
 
-#Lass mich nochmal probieren
-#Kann ich nicht löschen
